535dab89808aa6346ef7ff5b092a1499e5706481
Ajinkya.Aher <ajinkya.aher@esds.co.in>
2023-08-08 17:03:40 +0530

    Removed the unwanted files

--- backup MiSSing/MissingIR/app/src/main/java/com/ajinkya/missingir/Credential.java
+++
@@ -1,7 +0,0 @@
-package com.ajinkya.missingir;
-
-public class Credential {
-    public static final String[] stringsGender={"Select Gender","Male","Female"};
-
-    public static final String[] stringsDesignation={"Select Post","Constable","PI","SP","Head Constable"};
-}
